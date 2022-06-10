8067da3d0da80b96df58910e9acca6964adb4427
heyhocombo <heyhocombo@gmail.com>
2022-06-10 21:34:26 +0300

    10.06

--- Reverse Words/Model/DefaultCaseManager.swift
+++ Reverse Words/Model/DefaultCaseManager.swift
@@ -10,15 +10,19 @@ import UIKit
 final class DefaultCaseManager {
     private func reverseOnlyLetters_SeparateString(stringElement: String) -> String {
         var result = Array(stringElement)
-        var i: Int = 0, j: Int = result.count - 1
-          while i < j {
-              while i < j { if result[i].isLetter { break }; i += 1 }
-              while i < j { if result[j].isLetter { break }; j -= 1 }
-              let swap = result[i]; result[i] = result[j]; result[j] = swap
-              i += 1; j -= 1
-          }
-          return String(result)
-      }
+        var i: Int = 0
+        var j: Int = result.count - 1
+        while i < j {
+            while i < j { if result[i].isLetter { break }; i += 1 }
+            while i < j { if result[j].isLetter { break }; j -= 1 }
+            let swap = result[i]
+            result[i] = result[j]
+            result[j] = swap
+            i += 1
+            j -= 1
+        }
+        return String(result)
+    }
     
     func reververseText(Fulltext: String) -> String {
         let sampleText = Fulltext
@@ -26,5 +30,5 @@ final class DefaultCaseManager {
         let reversedArray = sampleArray.map{reverseOnlyLetters_SeparateString(stringElement: $0)}
         return reversedArray.joined(separator: " ")
     }
-    }
+}
 

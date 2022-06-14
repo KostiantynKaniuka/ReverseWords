8f2687f5be851a3ad89619f3eaf667ada098f999
heyhocombo <heyhocombo@gmail.com>
2022-06-14 18:35:18 +0300

    UnitTestEdit

--- Reverse Words Unit Tests/Reverse_Words_Tests.swift
+++ Reverse Words Unit Tests/Reverse_Words_Tests.swift
@@ -9,41 +9,79 @@ import XCTest
 @testable import Reverse_Words
 
 class Reverse_Words_Tests: XCTestCase {
-    var sut: DefaultCaseManager!
+    var sutDefault: DefaultCaseManager!
+    var sutCustom: CustomCaseManager!
     
     override func tearDownWithError() throws {
-        sut = nil
+        sutDefault = nil
+        sutCustom = nil
     }
     
     func testIf_InDefaultCaseTheNubers_Are_Ignored() throws {
         //Given
-        sut = DefaultCaseManager()
+        sutDefault = DefaultCaseManager()
         let input = "Foxminded cool 24/7"
         let expectedOutput = "dednimxoF looc 24/7"
         //When
-        let reverseAction = sut.reververseText(Fulltext: input)
+        let reverseAction = sutDefault.reververseText(Fulltext: input)
         //Then
         XCTAssertEqual(expectedOutput, reverseAction)
     }
     
-    func testIfReverseMethodIsWorking() throws {
+    func testIf_DefaultCase_ReverseMethodIsWorking() throws {
         //Given
-        sut = DefaultCaseManager()
+        sutDefault = DefaultCaseManager()
         let input = "abcd efgh"
         let expectedOutput = "dcba hgfe"
         //When
-        let reverseAction = sut.reververseText(Fulltext: input)
+        let reverseAction = sutDefault.reververseText(Fulltext: input)
         //Then
         XCTAssertEqual(expectedOutput, reverseAction)
     }
     
-    func testIfNumbersAreReversed() throws {
+    func testIf_DefaultCase_NumbersAreReversed() throws {
         //Given
-        sut = DefaultCaseManager()
+        sutDefault = DefaultCaseManager()
         let input = "a1bcd efg!h"
         let expectedOutput = "d1cba hgf!e"
         //When
-        let reverseAction = sut.reververseText(Fulltext: input)
+        let reverseAction = sutDefault.reververseText(Fulltext: input)
+        //Then
+        XCTAssertEqual(expectedOutput, reverseAction)
+    }
+    
+    func testIfRuleOfExclusionCharacterSetDataIsWorhing() throws {
+        //Given
+        sutCustom = CustomCaseManager()
+        let input = "Foxminded cool 24/7"
+        let exclusionCharacters = "xl"
+        let expectedOutput = "dexdnimoF oocl 7/42"
+        //When
+        let reverseAction = sutCustom.reverseWithoutFilter(fullText: input, textToIgnore: exclusionCharacters)
+        //Then
+        XCTAssertEqual(expectedOutput, reverseAction)
+    }
+    
+    func testCustomReverseMethod_WihoutExclusionCharactersSet() throws {
+        //Given
+        sutCustom = CustomCaseManager()
+        let input = "abcd efgh"
+        let exclusionCharacters = "xl"
+        let expectedOutput = "dcba hgfe"
+        //When
+        let reverseAction = sutCustom.reverseWithoutFilter(fullText: input, textToIgnore: exclusionCharacters)
+        //Then
+        XCTAssertEqual(expectedOutput, reverseAction)
+    }
+    
+    func testCustomReverseMethod_CointainOnly_One_ExclusionCharacterFromData() throws {
+        //Given
+        sutCustom = CustomCaseManager()
+        let input = "a1bcd efglh"
+        let exclusionCharacters = "xl"
+        let expectedOutput = "dcb1a hgfle"
+        //When
+        let reverseAction = sutCustom.reverseWithoutFilter(fullText: input, textToIgnore: exclusionCharacters)
         //Then
         XCTAssertEqual(expectedOutput, reverseAction)
     }
